7508e0d1aec36326b6d233c05792de35b2e12542
Michael Hartung <hartungm4@gmail.com>
2015-10-02 22:46:22 -0400

    Reads Info based on default template in Parse
    Will move on to beacon setup next

---
+++ dev/ios/MuseBeacon/MuseBeacon/SlideMenuTableCell.swift
@@ -0,0 +1,19 @@
+//
+//  SlideMenuTableCell.swift
+//  MuseBeacon
+//
+//  Created by Michael Hartung on 10/2/15.
+//  Copyright © 2015 hartung-michael. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+
+class SlideMenuTableCell: PFTableViewCell
+{
+	@IBOutlet weak var titleLabel: UILabel!
+	var slideText: String = ""
+	var slideImage: PFFile = PFFile()
+	var type: String = ""
+}
--- dev/ios/MuseBeacon/MuseBeacon/SliderMenuViewController.swift
+++ dev/ios/MuseBeacon/MuseBeacon/SliderMenuViewController.swift
@@ -46,9 +46,47 @@ class SliderMenuViewController: PFQueryTableViewController {
 	
 	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell?
 	{
-		let cell = tableView.dequeueReusableCellWithIdentifier("cell") as? PFTableViewCell
-		cell!.textLabel?.text = object?.objectForKey("Title") as? String
+		let cell = tableView.dequeueReusableCellWithIdentifier("cell") as? SlideMenuTableCell
+		cell!.type = "DefaultTemplate"
+		if( object?.objectForKey("Title") != nil )
+		{
+			cell!.titleLabel?.text = object?.objectForKey("Title") as? String
+		}
+		if( object?.objectForKey("Text") != nil )
+		{
+			cell!.slideText = object?.objectForKey("Text") as! String
+		}
+		if( object?.objectForKey("Image") != nil )
+		{
+			cell!.slideImage = object?.objectForKey("Image") as! PFFile
+		}
 		return cell
 	}
 	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+	{
+		let navController = segue.destinationViewController as! UINavigationController
+		let indexPath = self.tableView?.indexPathForSelectedRow as NSIndexPath!
+		let selectedCell = self.tableView?.cellForRowAtIndexPath(indexPath) as! SlideMenuTableCell
+		if ( selectedCell.type == "DefaultTemplate")
+		{
+			let destination : DefaultTemplate = navController.childViewControllers[0] as! DefaultTemplate
+			print(selectedCell.titleLabel.text!)
+			print(selectedCell.slideText)
+			destination.exhibitTitle = selectedCell.titleLabel.text!
+			destination.exhibitText = selectedCell.slideText
+			selectedCell.slideImage.getDataInBackgroundWithBlock
+			{
+				(imageData: NSData?, error: NSError?) -> Void in
+				if error == nil
+				{
+					if let imageData = imageData
+					{
+						destination.image = UIImage(data: imageData)!
+					}
+				}
+			}
+			
+		}
+	}
 }
