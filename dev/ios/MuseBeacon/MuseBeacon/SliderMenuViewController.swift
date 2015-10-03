e4202671a5c0a8ce1dcabcdf36434bb49d3d8c4b
Michael Hartung <hartungm4@gmail.com>
2015-10-03 01:32:56 -0400

    Added Beacon based exhibit presentation

--- dev/ios/MuseBeacon/MuseBeacon/SliderMenuViewController.swift
+++ dev/ios/MuseBeacon/MuseBeacon/SliderMenuViewController.swift
@@ -10,19 +10,53 @@ import UIKit
 import Parse
 import Bolts
 import ParseUI
+import CoreLocation
 
-class SliderMenuViewController: PFQueryTableViewController {
+class SliderMenuViewController: PFQueryTableViewController, CLLocationManagerDelegate
+{
+	
+	let locationManager = CLLocationManager()
+	var beaconArray: [String] = []
+	let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, identifier: "Estimotes")
 
 	override func viewWillAppear(animated: Bool)
 	{
 		super.viewWillAppear(animated)
 	}
 	
+	override func viewDidLoad()
+	{
+		locationManager.delegate = self
+		if(CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse)
+		{
+			locationManager.requestWhenInUseAuthorization()
+		}
+		locationManager.startRangingBeaconsInRegion(region)
+	}
+	
+	func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion)
+	{
+		// Code for beacon ranging
+		let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+		var knownBeaconArray: [String] = []
+		for beacon in knownBeacons
+		{
+			let beaconString = "\(beacon.major)-\(beacon.minor)"
+			print(beaconString)
+			knownBeaconArray.append(beaconString)
+		}
+		if knownBeaconArray != beaconArray
+		{
+			beaconArray = knownBeaconArray
+			self.tableView.reloadData()
+		}
+	}
+	
 	override func queryForTable() -> PFQuery
 	{
-		let queryArray = ["10", "20"]
+//		let queryArray = ["10", "20"]
 		let query = PFQuery(className: "DefaultTemplate")
-		query.whereKey("BeaconID", containedIn: queryArray)
+		query.whereKey("BeaconID", containedIn: beaconArray)
 		return query
 	}
 	
