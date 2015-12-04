cf63490b08d058c45296a3d7c0b91f890089ab7e
Dan Hoover <hooverd@mail.gvsu.edu>
2015-12-03 23:58:18 -0500

    split Parse functions into two files

--- dev/alt-web/public/javascript/parse.js
+++ dev/alt-web/public/javascript/parse.js
@@ -1,31 +1,13 @@
-function beaconDisplay() {
-  // appID and key for MuseBeacon's Parse Database
-  // var appID = // Get from user's login?
-  var appID = "BYMFAF3TttqN60BhZ0dykTDeUsFhB2OEtpovqLVT";
-  var javascriptKey = "PPQqXCZRwyBstXvoMtF2M9hwZecwZjqivq98w7dB";
-  
-  Parse.initialize(appID, javascriptKey);
-
-  var DefaultTemplate = Parse.Object.extend("DefaultTemplate");
-  var query = new Parse.Query(DefaultTemplate);
-
-  query.each(
-    function myF(object){
-		$(".sidebar col-md-2").append("<div class='row sidebar-content exhibit'>"
-			+ "<input type='hidden' name='objectID' value=" + object.id + "/>"
-			+ "<input type='hidden' name='text' value=" + object.get('Text') + "/>"
-			+ "<div class='col-md-6 name'><h4>" + object.get('Title') + "</h4></div>"
-			+ "<div class='col-md-6 collection'><h4>" + object.get('Collection') + "</h4></div></div>");
-    });
-}
-
 // needs paramater to determine this object
 // update in home.jade
-function submitChanges(beaconEntry) {
+function submitChanges(beaconID) {
 	var name = document.getElementsByName('exhibitname');
 	var collectionName = document.getElementsByName('collectionname');
 	var description = document.getElementsByName('text');
 	
+  var BeaconEntry = Parse.Object.extend("DefaultTemplate");
+  var BeaconEntry = new DefaultTemplate();
+
 	// better to compare with parse and see if a change was made? 
 	// or just resend the data anyways?
 	beaconEntry.set('Title', name);
---
+++ dev/alt-web/public/javascript/parseDisplay.js
@@ -0,0 +1,20 @@
+function beaconDisplay() {
+  // appID and key for MuseBeacon's Parse Database
+  // var appID = // Get from user's login?
+  var appID = "BYMFAF3TttqN60BhZ0dykTDeUsFhB2OEtpovqLVT";
+  var javascriptKey = "PPQqXCZRwyBstXvoMtF2M9hwZecwZjqivq98w7dB";
+  
+  Parse.initialize(appID, javascriptKey);
+
+  var DefaultTemplate = Parse.Object.extend("DefaultTemplate");
+  var query = new Parse.Query(DefaultTemplate);
+
+  query.each(
+    function myF(object){
+		$(".sidebar col-md-2").append("<div class='row sidebar-content exhibit'>"
+			+ "<input type='hidden' name='objectID' value=" + object.id + "/>"
+			+ "<input type='hidden' name='text' value=" + object.get('Text') + "/>"
+			+ "<div class='col-md-6 name'><h4>" + object.get('Title') + "</h4></div>"
+			+ "<div class='col-md-6 collection'><h4>" + object.get('Collection') + "</h4></div></div>");
+    });
+}
