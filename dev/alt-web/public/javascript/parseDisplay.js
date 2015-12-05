6dec7d3020c3d949fac16985ab0ea0935df231d2
Dan Hoover <hooverd@mail.gvsu.edu>
2015-12-05 11:32:52 -0500

    missed a file

--- dev/alt-web/public/javascript/parseDisplay.js
+++ dev/alt-web/public/javascript/parseDisplay.js
@@ -13,6 +13,7 @@ $(function(){
       var exhibitText = object.get('Text');
       console.log(exhibitText);
       $(".sidebar").append("<div class='row sidebar-content exhibit'>"
+        + "<input type='hidden' name='objectID' value=" + object.id + ">"
         + "<input type='hidden' name='text' value='" + exhibitText + "'>"
         + "<div class='col-md-6 name'><h4>" + object.get('Title') + "</h4></div>"
         + "<div class='col-md-6 collection'><h4>" + object.get('Collection') + "</h4></div></div>");
